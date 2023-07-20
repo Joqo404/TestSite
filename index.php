8269a7675117f5dcf6d36cf9279bef5ca4a03fc1
Egor Smirnov <desmond.de@yandex.ru>
2023-07-20 23:18:26 +0700

    update index.php, add style

--- index.php
+++ index.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1> ПЕРВЫЙ КОММИТ </h1>
+    <div class = "TittleImg">
+        <img src="image\Logo.png" id="Logo">
+        <img src="image\Illustration.png" id="Illustration">
+    </div>
+    <div class = "Singin">
+        <div><p>Email</p><input type="text" name="login"></div>
+        <div><p>Password</p><input type="password" name="password"></div>
+        <div><button> Sing in</button></div>
+    </div>
 </body>
 </html>
