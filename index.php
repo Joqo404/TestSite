3a0c1d599ff59b7c0f09139b461e53e7dfd96866
yungtonym <aleksklopot@gmail.com>
2023-07-21 18:26:16 +0300

    Add sign in page

--- index.php
+++ index.php
@@ -6,6 +6,24 @@
         <title>test page</title>
     </head>
     <body>
-        
+        <div id="wrapper">
+            <div id="images" class="main">
+                <img src="images/logo.svg" id="logo">
+                <img src="images/illustration.svg" id="illustration">
+            </div>
+            <div id="sign-in" class="main">
+                <div id="sign-in-block">
+                    <h4>Sign in</h4>
+                    <p>Email</p>
+                    <input type="text" name="email-input" placeholder="input your email in here">
+                    <p>Password</p>
+                    <input type="password" name="password" id="password-input" placeholder="input your password in here">
+                    <button id="show-password-button" onclick="return show_hide_password(this);"><img src="images/eye-no.svg"></button>
+                    <a id="forgot-password-text" href="https://www.google.com/">Forgot password?</a>
+                    <button id="sign-in-button">Sign in</button>
+                    <script src="scripts/main.js"></script> 
+                </div>
+            </div>
+        </div>
     </body>
 </html>
