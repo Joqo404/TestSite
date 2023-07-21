f9ec594b7f70f8358e65de5f44ae5f5b142b346c
yungtonym <aleksklopot@gmail.com>
2023-07-21 11:16:57 +0300

    Add link to css file

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html>
     <head>
+        <link href="./styles/style.css" rel="stylesheet" type="text/css" />
         <meta charset="utf-8" />
         <title>test page</title>
     </head>
