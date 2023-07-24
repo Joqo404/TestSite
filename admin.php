04396c29f14d6b97a7a804fd2be46679249a4ec4
Egor Smirnov <desmond.de@yandex.ru>
2023-07-24 23:42:05 +0700

    create header

--- admin.php
+++ admin.php
@@ -7,6 +7,13 @@
     <link rel="stylesheet" type="text/css" href="./style/adminStyle.css">
 </head>
 <body>
-    
+    <header> 
+        <div class="header-divs">
+            <button id="header-buttons-employees" onclick ="headerbuttons()">Employees</button>
+            <button class="header-buttons" id="header-buttons-currencies" onclick ="headerbuttons()">Currencies</button>
+            <button class="header-buttons" id="header-buttons-countries" onclick ="headerbuttons()">Countries</button>
+        </div>
+    </header>
+    <script type="text/javascript" src="js/admin.js"></script>
 </body>
 </html>
---
+++ js/admin.js
@@ -0,0 +1,6 @@
+function headerbuttons() {
+	var inputEmployees = document.getElementById('header-buttons-employees');
+	var inputCurrencies = document.getElementById('')
+	inputEmployees.style.backgroundColor = '#EBFAFA';
+	
+}
